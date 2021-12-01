7a8802476db631af120918f8986de020c7d06c6a
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2021-12-01 15:58:04 +0900

    event handling

--- main.js
+++ main.js
@@ -11,5 +11,16 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        deleteFromCart() {
+            this.cart = this.cart > 0 ? this.cart - 1 : 0
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
     }
 })
